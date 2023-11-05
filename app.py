813e442285dab7aa2f00f1026be767e48f961f11
NoahChristiansen <noah.a.christiansen@gmail.com>
2023-11-04 23:33:10 -0400

    adding requirements

--- app.py
+++ app.py
@@ -22,25 +22,25 @@ df = get_df()
 colormap = branca.colormap.LinearColormap(
     vmin=df["age"].quantile(0.0),
     vmax=df["age"].quantile(1),
-    colors=["blue", "purple", "red"],
+    colors=["blue", 'mediumslateblue', "darkorchid",'mediumvioletred', "red"],
     caption="Age of Building",
 )
 
 m = folium.Map(location=[40.6602,-73.969749], zoom_start=12, tiles = "CartoDB positron")
 
 popup = GeoJsonPopup(
-    fields=['property_name_text', 'property_total_unit_count'],
-    aliases=['Property Name ', 'Total Unit Count\n'],
+    fields=['property_name_text', 'age','property_total_unit_count'],
+    aliases=['Property Name','Building Age', 'Total Units'],
     localize=True,
     labels=True,
     style="background-color: yellow;",
 )
 
 tooltip = GeoJsonTooltip(
-    fields=['property_name_text', 'property_total_unit_count', 'owner_organization_name',
+    fields=['property_name_text', 'age','property_total_unit_count', 'owner_organization_name',
             'SPONSOR', 'Natural Gas Use (kBtu)', 'Electricity Use - Grid Purchase (kWh)','LL84 Total GHG Emissions (MTCO2e)', 'Energy Star Score',
             'Site Energy Unit Intensity (EUI) (kBtu/sqft)'],
-    aliases=['Property Name', 'Total Unit Count ', 'Owner Organization ',
+    aliases=['Property Name','Building Age', 'Total Unit Count ', 'Owner Organization ',
             'Sponsor', 'Natural Gas Use (kBtu) ', 'Electricity Use - Grid Purchase (kWh) ','LL84 Total GHG Emissions (MTCO2e) ', 'Energy Star Score ',
             'Site Energy Unit Intensity (EUI) (kBtu/sqft) '],
     localize=True,
@@ -80,4 +80,4 @@ st.write('Currently Selected Property:')
 if output['last_active_drawing'] is not None:
     st.dataframe(output['last_active_drawing']['properties'],
                  column_config = {'':'Column'},hide_index = True,
-                 )
+                 use_container_width = True)
